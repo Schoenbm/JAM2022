6821f06242577fb71b599b573f2f74ac18ea63b4
Hugo Guerif <hugo.guerif@edu.esiee.fr>
2022-04-03 12:47:22 +0200

    Meteors scale with health

--- Assets/scripts/MeteorManager.cs
+++ Assets/scripts/MeteorManager.cs
@@ -7,14 +7,39 @@ public class MeteorManager : MonoBehaviour
     public GameManager gameManager;
     public GameObject MeteorPrefab;
     public GameObject parent;
+
+    public GameObject earthCore;
     
-    public float timeBetweenMeteors = 5f;
+    public float timeBetweenMeteors;
+    public float maxTimeBetweenMeteors;
+    public float minTimeBetweenMeteors;
     float timeBeforeNextMeteor = 0f;
-    int amount = 5; //Need to scale it to difficulty or time elapsed.
+    int amount; //Need to scale it to difficulty or time elapsed.
+    public int maxAmount;
+    public int minAmount;
+    public Planet myPlanet;
+
+    void Start(){
+        maxTimeBetweenMeteors = 3f;
+        minTimeBetweenMeteors = 0.25f;
+        timeBetweenMeteors = maxTimeBetweenMeteors;
+        minAmount = 4;
+        amount = minAmount;
+        maxAmount = 10;
+        //myPlanet = earthCore.transform.GetChild(0).GetComponent<Planet>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(myPlanet.health/myPlanet.maxHealth);
+        amount = Mathf.Max(minAmount, maxAmount - Mathf.FloorToInt(maxAmount*(myPlanet.health/myPlanet.maxHealth)));
+        if(amount > maxAmount){
+            amount = maxAmount;
+        }
+        
+        timeBetweenMeteors = Mathf.Max(minTimeBetweenMeteors,(maxTimeBetweenMeteors*(myPlanet.health/myPlanet.maxHealth)));
+
         if(!gameManager.gameOver && timeBeforeNextMeteor <= 0f){
             for(int i = 0; i < amount; i++){
                 timeBeforeNextMeteor = timeBetweenMeteors;
