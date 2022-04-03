a354cb4f2df27e19b09541532929d7f84caceede
Schoenbm <50584669+Schoenbm@users.noreply.github.com>
2022-04-03 18:21:41 +0200

    Changes

--- Assets/scripts/Collectible.cs
+++ Assets/scripts/Collectible.cs
@@ -6,23 +6,41 @@ public class Collectible : MonoBehaviour
 {
     public float lifeSpan;
     float timeToDespawn;
-
+    SpriteRenderer spr;
     GameManager gameManager;
 
+    public float SpinSpeed = 0;
+    public float WaveAmplitude = 0;
     public Platform platform;
     void Start()
     {
+        spr = this.gameObject.GetComponent<SpriteRenderer>();
         timeToDespawn = lifeSpan;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        Wave();
+        Spin();
         if(timeToDespawn <= 0){
             timeToDespawn = lifeSpan;
             platform.hasItem = false;
             Destroy(this.gameObject);
         }
         timeToDespawn -= Time.deltaTime;
+        spr.color = new Color(1, 1, 1, Mathf.Min(3 *timeToDespawn / lifeSpan));
+    }
+
+    public void Spin()
+    {
+        Vector3 rotation = new Vector3(0, 0,  SpinSpeed * Time.deltaTime);
+        this.transform.eulerAngles += rotation;
+    }
+
+    public void Wave()
+    {
+        Vector3 translation = new Vector3(0, 0, WaveAmplitude * Time.time);
+        this.transform.position += translation;
     }
 }
