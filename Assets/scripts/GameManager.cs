d9b3705b37991561baf19ab38285e8a2ae63d64d
Hugo Guerif <hugo.guerif@edu.esiee.fr>
2022-04-03 23:33:32 +0200

    Game End + Earth Explosion

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -205,6 +205,27 @@ public class GameManager : MonoBehaviour
         foreach(GameObject go in scrapItems){
             Destroy(go);
         }
+        GameObject[] meteors = GameObject.FindGameObjectsWithTag("Killable");
+        foreach(GameObject go in meteors){
+            Destroy(go);
+        }
+        StartCoroutine(DezoomCamera());
+        earthCore.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        earthCore.transform.GetChild(0).GetChild(0).gameObject.GetComponent<ParticleSystem>().Play();
+        earthCore.transform.GetChild(0).GetChild(1).gameObject.GetComponent<ParticleSystem>().Play();
+        StartCoroutine(DestroyEverything());
     }
+
+    IEnumerator DezoomCamera(){
+        if(CMcamera.transform.position.z > -250)
+            CMcamera.transform.position = CMcamera.transform.position + new Vector3(0, 0, -5);
+        yield return new WaitForSeconds(1f);
+    }
+
+    IEnumerator DestroyEverything(){
+        yield return new WaitForSeconds(5f);
+        Destroy(earthCore.gameObject);
+    }
+
 
 }
