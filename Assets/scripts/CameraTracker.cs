08cf4dc7b05a72caf3e03f0a2f592febc266402a
Schoenbm <50584669+Schoenbm@users.noreply.github.com>
2022-04-04 19:28:23 +0200

    Camera

--- Assets/scripts/CameraTracker.cs
+++ Assets/scripts/CameraTracker.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class CameraTracker : MonoBehaviour
 {
     public Movement player;
+    Rigidbody2D rb;
+    private void Start()
+    {
+        rb = this.gameObject.GetComponentInParent<Rigidbody2D>();
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.localPosition = new Vector3(Input.GetAxis("Horizontal") * 2, 0, 0);
+        float coef = player.gameObject.transform.transform.position.y - 29;
+        if(rb.velocity.y >= 0)
+            transform.localPosition = new Vector3(Input.GetAxis("Horizontal") * 2, rb.velocity.y/ 5 - coef/8, 0);
+        else
+            transform.localPosition = new Vector3(Input.GetAxis("Horizontal") * 2, rb.velocity.y / 2.7f - coef/8, 0);
     }
 }
